6a1ccd0d8990193e9afcd510aae351fb79b0e1f1
Nadya <nadin.ts@gmail.com>
2017-09-15 17:23:52 +0300

    initial project version

---
+++ hello-world/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Dropdown from './Dropdown.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        < Dropdown />
+      </div>
+    );
+  }
+}
+
+export default App;
